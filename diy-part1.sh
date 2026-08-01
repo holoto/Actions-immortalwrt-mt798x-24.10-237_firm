355fb1c7c6889f633f3929af7cbd690e2d8e5a60
holoto <laibin45@gmail.com>
2026-08-01 15:59:47 +0200

    sn-r1

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,16 +17,16 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-# rm -rf feeds/packages/lang/golang
-# git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
 
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 # git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
 
-# rm -rf feeds/luci/applications/luci-app-passwall
-# # git clone https://github.com/Openwrt-Passwall/openwrt-passwall package/passwall-luci
-# git clone https://github.com/Openwrt-Passwall/openwrt-passwall2 package/passwall2-luci
- sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+rm -rf feeds/luci/applications/luci-app-passwall
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall package/passwall-luci
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall2 package/passwall2-luci
+#  sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
 
 
